a7343ddd3f1a7ee3482b1c8de0e8771bf1a9b826
Mehdi-Choukri <medelmehdi.choukri@gmail.com>
2020-12-17 18:49:56 +0100

    push du brief java

--- src/Execution.java
+++ src/Execution.java
@@ -1,9 +1,11 @@
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 import javazoom.jl.decoder.JavaLayerException;
@@ -21,6 +23,9 @@ public class Execution implements InterListe {
 	static JLabel lapelTime;
 	String chemin;
 	static Player player;
+	static int cpt=0;
+	static JButton btn_valider;
+	static JLabel lapel_score;
 
 	public static void main(String[] args) {
 	 
@@ -63,7 +68,6 @@ public class Execution implements InterListe {
 			FileInputStream fileInputStrem = new FileInputStream(chemin);
 			player = new Player(fileInputStrem);
 			player.play(frames);
-			System.out.println("On !");
 			
 		}
 		catch(FileNotFoundException e)
@@ -161,7 +165,8 @@ public class Execution implements InterListe {
 		
 		remplirePanelNiveau(1);
 		
-		JButton btn_valider = new JButton("valider");
+
+		btn_valider = new JButton("valider");
 		btn_valider.setBounds(650, 500, 100, 30);
 		panel.add(btn_valider);
 		btn_valider.addActionListener(new ActionListener() {
@@ -169,27 +174,7 @@ public class Execution implements InterListe {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if(checkReponseAllQuestion(1)) {
-					if(calculeScore(1) >= 40) {
-
-						//affichage du resultat
-						System.out.println(nbrReponseCorrect(1,listeQuiz,listePlayer_QUIZ));
-						niveau2();
-						
-					}
-					else {
-						 try {
-								StopMusic();
-							} catch (JavaLayerException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						playerMusic("lose.mp3",200);
-						 String invisibleChar= "\u200e";
-							final ImageIcon icon = new ImageIcon("lose.gif");
-	                        JOptionPane.showMessageDialog(null,invisibleChar, "YOU LOST HAHAHAH", JOptionPane.INFORMATION_MESSAGE, icon);
-						
-	                    System.exit(0);
-					}
+					afficheCorrection(1);
 				}
 				else {
 					JOptionPane.showMessageDialog(null, "Merci de répondre à toutes les questions");
@@ -201,7 +186,7 @@ public class Execution implements InterListe {
 	public static void niveau2() {
 		
 		Quiz quiz1 = new Quiz("Après la compilation, un programme écrit en JAVA, il se transforme en programme en bytecode. Quelle est l’extension du programme en bytecode ?", ".Class", "aucun des choix", ".JAVA", ".Class");
-		Quiz quiz2 = new Quiz("Class Test{Public Test () {System.out.println(”Bonjour”);}public Test (int i) {this(); System.out.println(”Nous sommes en ”+i+”!”);}; }qu’affichera l’instruction suivante? Test t1=new Test (2018);", "Bonjour Nous sommes en 2018 !", "aucun des choix", "Bonjour Nous sommes en 2018 !", "Nous sommes en 2018 !");
+		Quiz quiz2 = new Quiz("Class Test{Public Test () {System.out.println(”Bonjour”);}public Test (int i) {this(); System.out.println(”Nous sommes en ”+i+”!”);}; }qu’affichera l’instruction suivante? Test t1=new Test (2020);", "Bonjour Nous sommes en 2020 !", "aucun des choix", "Bonjour Nous sommes en 2020 !", "Nous sommes en 2020 !");
 		Quiz quiz3 = new Quiz("Voici un constructeur de la classe Employee, y-a-t'il un problème Public void Employe(String n){Nom=n;}", "vrai", "vrai", "faux");
 		Quiz quiz4 = new Quiz("Pour spécifier que la variable ne pourra plus être modifiée et doit être initialisée dès sa déclaration, on la déclare comme une constante avec le mot réservé", "final", "aucun des choix", "final","const");
 		Quiz quiz5 = new Quiz("Dans une classe, on accède à ses variables grâce au mot clé", "this", "aucun des choix", "this", "super");
@@ -222,30 +207,7 @@ public class Execution implements InterListe {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if(checkReponseAllQuestion(2)) {
-					if(calculeScore(2) >= 60) {
-						/*
-						viderLesCollection();
-						
-						*/
-						//affichage du resultat
-						System.out.println(nbrReponseCorrect(2,listeQuiz,listePlayer_QUIZ));
-						niveau3();
-						
-					}
-					else {
-						 try {
-								StopMusic();
-							} catch (JavaLayerException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						playerMusic("lose.mp3",200);
-						 String invisibleChar= "\u200e";
-							final ImageIcon icon = new ImageIcon("lose.gif");
-	                        JOptionPane.showMessageDialog(null,invisibleChar, "YOU LOST HAHAHAH ", JOptionPane.INFORMATION_MESSAGE, icon);
-						
-	                    System.exit(0);
-					}
+					afficheCorrection(2);
 				}
 				else {
 					JOptionPane.showMessageDialog(null, "Merci de répondre à toutes les questions");
@@ -277,45 +239,7 @@ public class Execution implements InterListe {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if(checkReponseAllQuestion(3)) {
-					if(calculeScore(3) >= 80) {
-
-						//affichage du resultat
-						System.out.println(nbrReponseCorrect(3,listeQuiz,listePlayer_QUIZ));
-						t.stop();
-						try {
-							StopMusic();
-						} catch (JavaLayerException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-						playerMusic("win.mp3",300);
-	                  
-	                   String invisibleChar= "\u200e";
-						final ImageIcon icon = new ImageIcon("source.gif");
-                        JOptionPane.showMessageDialog(null,invisibleChar, "YOU WON ! ", JOptionPane.INFORMATION_MESSAGE, icon);
-						 try {
-								StopMusic();
-							} catch (JavaLayerException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						  System.exit(0);
-						
-					}
-					else {
-						 try {
-								StopMusic();
-							} catch (JavaLayerException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						playerMusic("lose.mp3",200);
-						 String invisibleChar= "\u200e";
-							final ImageIcon icon = new ImageIcon("lose.gif");
-	                        JOptionPane.showMessageDialog(null,invisibleChar, "YOU LOST HAHAHAH ", JOptionPane.INFORMATION_MESSAGE, icon);
-						
-	                    System.exit(0);
-					}
+					afficheCorrection(3);
 				}
 				else {
 					JOptionPane.showMessageDialog(null, "Merci de répondre à toutes les questions");
@@ -343,6 +267,8 @@ public class Execution implements InterListe {
 		return ret;
 		
 	}
+	
+	
 	
 	public static int nbrReponseCorrect(int niveau ,ArrayList<Quiz> q1,ArrayList<Player_QUIZ> pq)
 	{
@@ -406,9 +332,12 @@ public class Execution implements InterListe {
 		}
 		panel.removeAll();
 		panel.repaint();
+		lapel_score =new JLabel();
+		lapel_score.setBounds(700,30,70,20);
+		panel.add(lapel_score);
 		lapelTime.setBounds(700,10,70,20);
 		panel.add(lapelTime);
-		int y=20;
+		int y=40;
 		for(int i = start;i<end;i++) {
 			int x=30;
 			JLabel lapel_question = new JLabel("<html><p>"+listeQuiz.get(i).getQuestion()+"</p></html>");
@@ -478,7 +407,6 @@ public class Execution implements InterListe {
 
 	public static int calculeScore(int niveau)
 	{
-		getreponses(niveau);
 		int start , end;
 		if(niveau == 1)
 		{
@@ -530,5 +458,143 @@ public class Execution implements InterListe {
 			}
 		}
 		return ret;
+	}
+	
+	public static void correction(int niveau){
+		int start , end;
+		if(niveau == 1)
+		{
+			start=0;
+			end  =5;
+ 
+		}
+		else if(niveau == 2)
+		{
+			start=5;
+			end  =10;
+		}
+		else
+		{
+			start=10;
+			end  =15;
+		}
+		for(int i = start;i<end;i++) {
+			if(listePlayer_QUIZ.get(i).isGoodchoice()) {
+				Enumeration<AbstractButton> radsV = listeButtonGroup.get(i).getElements();
+		        while (radsV.hasMoreElements()) {
+		            JRadioButton temp = (JRadioButton) radsV.nextElement();
+		            if (temp.isSelected()) {
+		                temp.setForeground(Color.green);
+		                break;
+		            }
+		        }
+			}
+			else{
+				Enumeration<AbstractButton> radsV = listeButtonGroup.get(i).getElements();
+		        while (radsV.hasMoreElements()) {
+		            JRadioButton temp = (JRadioButton) radsV.nextElement();
+		            if (temp.isSelected()) {
+		                temp.setForeground(Color.red);
+		            }
+		            else if(temp.getActionCommand() == listeQuiz.get(i).getReponse()) {
+		            	temp.setForeground(Color.green);
+		            }
+		        }
+			}
+		}
+	}
+	public static void afficheCorrection(int niveau) {
+			if(cpt==0) {
+				getreponses(niveau);
+				cpt=1;
+				btn_valider.setText("Suivant");
+				correction(niveau);
+				lapel_score.setText("score : " + calculeScore(niveau));
+			}
+			else {
+				cpt=0;
+				if(niveau==1) {
+					if(calculeScore(1) >= 40) {
+						
+						niveau2();
+						
+					}
+					else {
+						 try {
+								StopMusic();
+							} catch (JavaLayerException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						playerMusic("lose.mp3",100);
+						 String invisibleChar= "\u200e";
+							final ImageIcon icon = new ImageIcon("lose.gif");
+	                        JOptionPane.showMessageDialog(null,invisibleChar, "YOU LOST HAHAHAH", JOptionPane.INFORMATION_MESSAGE, icon);
+						
+	                    System.exit(0);
+					}
+				}
+				else if(niveau==2) {
+					if(calculeScore(2) >= 60) {
+						niveau3();
+						
+					}
+					else {
+						 try {
+								StopMusic();
+							} catch (JavaLayerException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						playerMusic("lose.mp3",100);
+						 String invisibleChar= "\u200e";
+							final ImageIcon icon = new ImageIcon("lose.gif");
+	                        JOptionPane.showMessageDialog(null,invisibleChar, "YOU LOST HAHAHAH ", JOptionPane.INFORMATION_MESSAGE, icon);
+						
+	                    System.exit(0);
+					}
+				}
+				else {
+					if(calculeScore(3) >= 80) {
+
+						//affichage du resultat
+						t.stop();
+						try {
+							StopMusic();
+						} catch (JavaLayerException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+						playerMusic("win.mp3",100);
+	                  
+	                   String invisibleChar= "\u200e";
+						final ImageIcon icon = new ImageIcon("source.gif");
+                        JOptionPane.showMessageDialog(null,invisibleChar, "YOU WON ! ", JOptionPane.INFORMATION_MESSAGE, icon);
+						 try {
+								StopMusic();
+							} catch (JavaLayerException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						  System.exit(0);
+						
+					}
+					else {
+						 try {
+								StopMusic();
+							} catch (JavaLayerException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						playerMusic("lose.mp3",100);
+						 String invisibleChar= "\u200e";
+							final ImageIcon icon = new ImageIcon("lose.gif");
+	                        JOptionPane.showMessageDialog(null,invisibleChar, "YOU LOST HAHAHAH ", JOptionPane.INFORMATION_MESSAGE, icon);
+						
+	                    System.exit(0);
+					}
+					
+				}
+		}
 	}
 }
